bf3818f5cc01061b1557c2ebed0da9126fe7a8aa
Bronyrus <bronyruschannel@gmail.com>
2019-08-27 12:57:37 +0300

    Update listOfClients.blade.php

--- resources/views/client/listOfClients.blade.php
+++ resources/views/client/listOfClients.blade.php
@@ -4,9 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @foreach($clients as $client)
-                {{ $client->login }}
-            @endforeach
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th><input type="checkbox" name="destroy-all-clients" class="js-destroy-all"/></th>
+                    <th>Логин</th>
+                    <th>Роль</th>
+                    <th>Дата создания</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($clients as $client)
+                    <tr>
+                        <td><input type="checkbox" data-client-id="{{ $client->id }}" name="destoy-client-{{ $client->id }}" class="js-destroy"/></td>
+                        <td><a href="client/{{ $client->id }}"> {{ $client->login }} </a></td>
+                        <td>{{ $client->role }}</td>
+                        <td>{{ $client->created_at->timezone('Europe/Moscow') }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
