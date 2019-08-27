a55e1385e9db8f26c68d9da335cc280d4bdcf70b
Bronyrus <bronyruschannel@gmail.com>
2019-08-27 12:55:36 +0300

    Update listOfClients.blade.php

--- resources/views/client/listOfClients.blade.php
+++ resources/views/client/listOfClients.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             @foreach($clients as $client)
-                {{ $client->name }}
+                {{ $client->login }}
             @endforeach
         </div>
     </div>
