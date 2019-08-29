a0224fe32d9105ed4ad6cf77df065f2ce180f901
Bronyrus <bronyruschannel@gmail.com>
2019-08-29 15:09:31 +0300

    Update 2019_08_22_123036_entrust_setup_tables.php

--- database/migrations/2019_08_22_123036_entrust_setup_tables.php
+++ database/migrations/2019_08_22_123036_entrust_setup_tables.php
@@ -11,6 +11,7 @@ class EntrustSetupTables extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('roles');
         // Create table for storing roles
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
@@ -20,6 +21,7 @@ class EntrustSetupTables extends Migration
             $table->timestamps();
         });
 
+        Schema::dropIfExists('role_user');
         // Create table for associating roles to users (Many-to-Many)
         Schema::create('role_user', function (Blueprint $table) {
             $table->integer('user_id')->unsigned();
@@ -33,6 +35,7 @@ class EntrustSetupTables extends Migration
             $table->primary(['user_id', 'role_id']);
         });
 
+        Schema::dropIfExists('permissions');
         // Create table for storing permissions
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
@@ -42,6 +45,7 @@ class EntrustSetupTables extends Migration
             $table->timestamps();
         });
 
+        Schema::dropIfExists('permission_role');
         // Create table for associating permissions to roles (Many-to-Many)
         Schema::create('permission_role', function (Blueprint $table) {
             $table->integer('permission_id')->unsigned();
