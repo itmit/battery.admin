c141a3f2399cddb6ffeddbd466650ae565024df3
Bronyrus <bronyruschannel@gmail.com>
2019-08-30 10:35:31 +0300

    Update ShipmentApiController.php

--- app/Http/Controllers/Api/ShipmentApiController.php
+++ app/Http/Controllers/Api/ShipmentApiController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
-// use App\Models\Client;
-// use Illuminate\Http\JsonResponse;
-// use Illuminate\Http\Request;
-// use Illuminate\Http\Response;
-// use Illuminate\Support\Carbon;
-// use Illuminate\Support\Facades\Auth;
-// use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Facades\Validator;
-// use Illuminate\Support\Facades\Storage;
-// use Illuminate\Support\Facades\DB;
+use App\Models\Client;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class ShipmentApiController extends ApiBaseController
 {
@@ -25,14 +25,14 @@ class ShipmentApiController extends ApiBaseController
     public function listOfDealers()
     { 
 
-        return 'a';
+        // return 'a';
 
-        // $dealers = Client::where('role', '=', 'delaer')->get();
+        $dealers = Client::where('role', '=', 'delaer')->get();
 
-        // return $this->sendResponse([
-        //     $dealers
-        // ],
-        //     'Updated');
+        return $this->sendResponse([
+            $dealers
+        ],
+            'Updated');
 
     }
     
