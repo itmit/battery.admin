81f392250c355afa32dd54a880e4c0e1e15bd2a2
Bronyrus <bronyruschannel@gmail.com>
2019-08-30 11:01:56 +0300

    Update ShipmentApiController.php

--- app/Http/Controllers/Api/ShipmentApiController.php
+++ app/Http/Controllers/Api/ShipmentApiController.php
@@ -27,7 +27,7 @@ class ShipmentApiController extends ApiBaseController
 
         // return 'a';
 
-        $dealers = Client::where('role', '=', 'delaer')->get()->toArray();
+        $dealers = Client::where('role', '=', 'dealer')->get()->toArray();
 
         return $this->sendResponse($dealers, 'Updated');
 
