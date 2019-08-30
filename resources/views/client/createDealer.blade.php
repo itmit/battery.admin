25783d9cb764a52a15f70e5f3e93cdf47e3f87d6
Bronyrus <bronyruschannel@gmail.com>
2019-08-30 17:18:58 +0300

    dsgg

--- resources/views/client/createDealer.blade.php
+++ resources/views/client/createDealer.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Создание клиента</h1>
     <div class="col-sm-12">
-        <form class="form-horizontal" method="POST" action="{{ route('') }}">
+        <form class="form-horizontal" method="POST" action="">
             {{ csrf_field() }}
 
             <div class="form-group{{ $errors->has('login') ? ' has-error' : '' }}">
