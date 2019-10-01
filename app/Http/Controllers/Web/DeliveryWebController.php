37795e94e1641b1c1f6fac65aa98f85052610aba
Bronyrus <bronyruschannel@gmail.com>
2019-10-01 14:01:25 +0300

    Update DeliveryWebController.php

--- app/Http/Controllers/Web/DeliveryWebController.php
+++ app/Http/Controllers/Web/DeliveryWebController.php
@@ -47,7 +47,7 @@ class DeliveryWebController extends Controller
             $handle = fopen($url, "r");
             $header = true;
 
-            return substr(strrchr($file, "/"), 1);
+            return strrchr(substr(strrchr($file, "/"), 1), ".");
 
             while ($csvLine = fgetcsv($handle, 1000, ";")) {
 
