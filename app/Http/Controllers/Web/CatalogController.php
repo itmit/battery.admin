501a6cf68c89676ccc2b6f16d3e36d8242bb1a5c
Bronyrus <bronyruschannel@gmail.com>
2020-03-23 16:09:34 +0300

    Update CatalogController.php

--- app/Http/Controllers/Web/CatalogController.php
+++ app/Http/Controllers/Web/CatalogController.php
@@ -119,7 +119,7 @@ class CatalogController extends Controller
 
         $cells = $spreadsheet->getActiveSheet()->getCellCollection();
 
-        for ($row = 84; $row <= 102; $row++){
+        for ($row = 107; $row <= 113; $row++){
             for ($col = 'B'; $col <= 'Q'; $col++) {
                 if($cells->get($col.$row) == null) $position[$col] = null;
                 else $position[$col] = $cells->get($col.$row)->getValue();
@@ -131,7 +131,7 @@ class CatalogController extends Controller
         foreach($result as $item)
         {
             Battery::create([
-                'category_id' => 4,
+                'category_id' => 5,
                 'tab_id' => $item["B"],
                 'neutral_id' => $item["C"],
                 'din_marking' => $item["D"],
