95f228f95c7f9d71e9651238d9deb012e007b448
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-07 15:00:51 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:checkDeliveries')->hourly();
+        $schedule->command('command:checkDeliveries')->everyTenMinutes();
     }
 
     /**
